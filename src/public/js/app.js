0aa3dd646d5bed0fbf261edfd63d3092a61770c9
LeeSoMyoung <so4644009@gmail.com>
2021-08-04 23:07:47 +0900

    socket 기본

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,21 @@
-// Front end
+// front-end
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open",()=>{
+    console.log('connected to the server');
+    socket.send('hello!');
+});
+
+ socket.addEventListener('message', (message) => {
+    const translatedMessageData = message.toString('utf8');
+    console.log(translatedMessageData);
+  });
+
+socket.addEventListener('close',()=>{
+    console.log('the server is disconnected.');
+});
+
+setTimeout(()=>{
+    socket.send("hello from the browser");
+},5000);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
 const app = express();
 
@@ -7,10 +9,30 @@ app.set('views', __dirname+"/views");
 
 app.use('/public',express.static(__dirname+'/public'));
 
-app.get("/",(req,res) => res.render("home"));
+app.get("/",(_,res) => res.render("home"));
+//app.get("/*",(_,res)=>res.redirect("/"));
+
 
 const SERVERNUM=3000;
-const server = `http://localhost:${SERVERNUM}`;
-const handleListen = () => console.log(`Listening on ${server}`);
+const serverAddress = `http://localhost:${SERVERNUM}`;
+const handleListen = () => console.log(`Listening on ${serverAddress}`);
 
+const server = http.createServer(app); // http 서버
+const wss = new WebSocket.Server({server}); // 웹 소켓 서버 : http 서버를 돌리면 웹 소켓 서버도 실행
+
+// 누가 서버와 연결되었을 때
+wss.on("connection", (socket)=>{
+    socket.on('close',()=>{
+        console.log('disconnected from the client');
+    });
+    socket.on('message',(message)=>{
+        console.log(message);
+    });
+    socket.send("hello!"); // 데이터를 socket에 보낸다.
+});
+
+server.listen(SERVERNUM, handleListen);
+
+/*
 app.listen(SERVERNUM, handleListen);
+*/
