957097d48383a7981f3bd87e5d96b56509d3fdb9
LeeSoMyoung <so4644009@gmail.com>
2021-08-05 19:14:07 +0900

    채팅하기&닉네임

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,21 +1,51 @@
 // front-end
 
+const messageList = document.querySelector('ul');
+const messageForm = document.querySelector('#message');
+const nickForm = document.querySelector('#nick');
+
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open",()=>{
     console.log('connected to the server');
-    socket.send('hello!');
+
 });
 
  socket.addEventListener('message', (message) => {
-    const translatedMessageData = message.toString('utf8');
-    console.log(translatedMessageData);
+    const li = document.createElement('li');
+    li.innerText=message.data;
+    messageList.append(li);
   });
 
 socket.addEventListener('close',()=>{
     console.log('the server is disconnected.');
 });
 
-setTimeout(()=>{
+/*setTimeout(()=>{
     socket.send("hello from the browser");
-},5000);
+},5000); */
+
+function makeMessage(type,payload){
+    const msg = {
+        type,
+        payload
+    };
+    return JSON.stringify(msg);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector('input');
+    socket.send(makeMessage("new message",input.value));
+    input.value=""; // 메세지가 전송되었으면 form을 비워준다.
+}
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector('input');
+    socket.send(makeMessage("nickname",input.value));
+    //input.innerText="";
+}
+
+messageForm.addEventListener('submit', handleSubmit);
+nickForm.addEventListener('submit',handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -4,31 +4,45 @@ import WebSocket from 'ws';
 
 const app = express();
 
-app.set('view engine','pug');
-app.set('views', __dirname+"/views");
+app.set('view engine', 'pug');
+app.set('views', __dirname + "/views");
 
-app.use('/public',express.static(__dirname+'/public'));
+app.use('/public', express.static(__dirname + '/public'));
 
-app.get("/",(_,res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
 //app.get("/*",(_,res)=>res.redirect("/"));
 
 
-const SERVERNUM=3000;
+const SERVERNUM = 3000;
 const serverAddress = `http://localhost:${SERVERNUM}`;
 const handleListen = () => console.log(`Listening on ${serverAddress}`);
 
 const server = http.createServer(app); // http 서버
-const wss = new WebSocket.Server({server}); // 웹 소켓 서버 : http 서버를 돌리면 웹 소켓 서버도 실행
+const wss = new WebSocket.Server({ server }); // 웹 소켓 서버 : http 서버를 돌리면 웹 소켓 서버도 실행
+
+const sockets = []; // 여러 사람이 서버에 들어올 수도 있으므로 그걸 저장하는 배열이 된다.
 
 // 누가 서버와 연결되었을 때
-wss.on("connection", (socket)=>{
-    socket.on('close',()=>{
+wss.on("connection", (socket) => {
+    sockets.push(socket); // 유저가 접속하면 접속 소켓 목록에 추가한다.
+    socket["nickname"]="Anon";
+    socket.on('close', () => {
         console.log('disconnected from the client');
     });
-    socket.on('message',(message)=>{
-        console.log(message);
+    socket.on('message', (message) => {
+        const msg = JSON.parse(message);
+        sockets.forEach((aSocket) => {
+            switch (msg.type) {
+                case 'new message':
+                    aSocket.send(`${socket.nickname}:${msg.payload}`);
+                    break;
+
+                case 'nickname':
+                    socket["nickname"]=msg.payload;
+                    break;
+            }
+        });
     });
-    socket.send("hello!"); // 데이터를 socket에 보낸다.
 });
 
 server.listen(SERVERNUM, handleListen);
