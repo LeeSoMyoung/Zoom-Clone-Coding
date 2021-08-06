7b83b291924bde5c1734e3a8710b61ff2e88e27c
LeeSoMyoung <so4644009@gmail.com>
2021-08-06 23:04:21 +0900

    socket.io 설치

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,51 +1,22 @@
 // front-end
 
-const messageList = document.querySelector('ul');
-const messageForm = document.querySelector('#message');
-const nickForm = document.querySelector('#nick');
+const socket = io(); // 자동적으로 서버가 돌아가는 것을 캐치한다.
 
-const socket = new WebSocket(`ws://${window.location.host}`);
+const welcome = document.querySelector('#welcome');
+const form = welcome.querySelector('form');
 
-socket.addEventListener("open",()=>{
-    console.log('connected to the server');
-
-});
-
- socket.addEventListener('message', (message) => {
-    const li = document.createElement('li');
-    li.innerText=message.data;
-    messageList.append(li);
-  });
-
-socket.addEventListener('close',()=>{
-    console.log('the server is disconnected.');
-});
-
-/*setTimeout(()=>{
-    socket.send("hello from the browser");
-},5000); */
-
-function makeMessage(type,payload){
-    const msg = {
-        type,
-        payload
-    };
-    return JSON.stringify(msg);
-}
-
-function handleSubmit(event){
+function handleRoomSubmit(event){
     event.preventDefault();
-    const input = messageForm.querySelector('input');
-    socket.send(makeMessage("new message",input.value));
-    input.value=""; // 메세지가 전송되었으면 form을 비워준다.
+    const input = form.querySelector('input');
+    socket.emit('enter_room', 
+    {
+        payload:input.value
+    },
+    ()=>{
+        console.log('콜백 함수');
+    }
+    );
+    input.value = "";
 }
 
-function handleNickSubmit(event){
-    event.preventDefault();
-    const input = nickForm.querySelector('input');
-    socket.send(makeMessage("nickname",input.value));
-    //input.innerText="";
-}
-
-messageForm.addEventListener('submit', handleSubmit);
-nickForm.addEventListener('submit',handleNickSubmit);
+form.addEventListener('submit',handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import http from 'http';
 import WebSocket from 'ws';
+import SocketIo from 'socket.io';
+import { Socket } from 'dgram';
 
 const app = express();
 
@@ -18,6 +20,18 @@ const serverAddress = `http://localhost:${SERVERNUM}`;
 const handleListen = () => console.log(`Listening on ${serverAddress}`);
 
 const server = http.createServer(app); // http 서버
+const io = SocketIo(server);
+
+io.on('connection',(socket)=>{
+    
+    socket.on('enter_room',(msg, done)=>{
+        console.log(msg);
+        setTimeout(done,2000);
+    });
+
+});
+
+/*
 const wss = new WebSocket.Server({ server }); // 웹 소켓 서버 : http 서버를 돌리면 웹 소켓 서버도 실행
 
 const sockets = []; // 여러 사람이 서버에 들어올 수도 있으므로 그걸 저장하는 배열이 된다.
@@ -50,3 +64,6 @@ server.listen(SERVERNUM, handleListen);
 /*
 app.listen(SERVERNUM, handleListen);
 */
+
+server.listen(SERVERNUM, handleListen);
+
