7a8bfac5e67db2a107478a6312fb3ba39f2ecf13
LeeSoMyoung <so4644009@gmail.com>
2021-08-11 17:38:12 +0900

    stun server & ice candidate

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -69,6 +69,13 @@ async function getMedia(deviceId){
 
 async function handleCameraChange(){
     await getMedia(camerasSelect.value);
+    if(myPeerConnection){
+        // myPeerConnection이 존재
+        const videoTrack = myStream.getVideoTracks()[0];
+        const videoSender = myPeerConnection.getSenders()
+        .find((sender)=>sender.track.kind === 'video');
+        videoSender.replaceTrack(videoTrack);
+    }
 }
 
 function handleMuteClick(){
@@ -108,17 +115,18 @@ camerasSelect.addEventListener('input',handleCameraChange);
 
 const welcomeForm = welcome.querySelector('form');
 
-async function startMedia(){
+async function initCall(){
     welcome.hidden=true;
     call.hidden=false;
     await getMedia();
     makeConnection();
 }
 
-function handleWelcomeSubmit(event){
+async function handleWelcomeSubmit(event){
     event.preventDefault();
     const input = welcome.querySelector('input');
-    socket.emit('join_room',input.value, startMedia);
+    await initCall();
+    socket.emit('join_room',input.value);
     roomName = input.value;
     input.value = "";
 }
@@ -127,21 +135,57 @@ welcomeForm.addEventListener('submit',handleWelcomeSubmit);
 
 // 방 참가
 
+// 이미 입장해 있는 참가자들이 실행
 socket.on('welcome', async ()=>{
     const offer = await myPeerConnection.createOffer();
     myPeerConnection.setLocalDescription(offer);
     socket.emit('offer',offer,roomName);
 });
 
-socket.on('offer', (offer)=>{
-    console.log(offer);
+// 최신 참가자한테 실행
+socket.on('offer', async (offer)=>{
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer);
+    socket.emit('answer',answer, roomName);
+});
+
+socket.on('answer',(answer)=>{
+    myPeerConnection.setLocalDescription(answer);
+});
+
+socket.on('ice',(ice)=>{
+    myPeerConnection.addIceCandidate(ice);
 });
 
 // RTC
 
 function makeConnection(){
-    myPeerConnection = new RTCPeerConnection();
+    myPeerConnection = new RTCPeerConnection({
+        iceServers: [
+            {
+              urls: [
+                "stun:stun.l.google.com:19302",
+                "stun:stun1.l.google.com:19302",
+                "stun:stun2.l.google.com:19302",
+                "stun:stun3.l.google.com:19302",
+                "stun:stun4.l.google.com:19302",
+              ],
+            },
+          ],
+    });
+    myPeerConnection.addEventListener('icecandidate',handleIce);
+    myPeerConnection.addEventListener('addstream',handleAddStream);
     myStream.getTracks().forEach((track)=>{
         myPeerConnection.addTrack(track,myStream);
     });
+}
+
+function handleIce(data){
+    socket.emit('ice',data.candidate, roomName);
+}
+
+function handleAddStream(data){
+    const peersStream = document.querySelector('#peersStream');
+    peersStream.srcObject=data.stream;
 }
